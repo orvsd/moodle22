58a4d3c6aa54d8bc1e42e1838b74557cbea87513
Rudy Grigar <basic@osuosl.org>
2012-10-10 03:37:21 +0000

    orvsd: new config.php for automated site deploy

---
+++ config.php
@@ -0,0 +1,50 @@
+<?PHP
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// Moodle configuration file                                             //
+//                                                                       //
+// This file should be renamed "config.php" in the top-level directory   //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.org                                            //
+//                                                                       //
+// Copyright (C) 1999 onwards  Martin Dougiamas  http://moodle.com       //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 3 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+unset($CFG);  // Ignore this line
+global $CFG;  // This is necessary here for PHPUnit execution
+$CFG = new stdClass();
+
+//=========================================================================
+// 1. ORVSD CONFIG
+//=========================================================================
+// Include relevant configuration from glusterfs mount.
+require_once('/data/moodledata/' . $_SERVER['USER'] . '/' . $_SERVER['FQDN'] . '/config.php');
+// Enable when using external SSL appliance for performance reasons.
+// Please note that site may be accessible via https: or https:, but not both!
+      $CFG->sslproxy = true;
+
+//=========================================================================
+// ALL DONE!  To continue installation, visit your main page with a browser
+//=========================================================================
+
+require_once(dirname(__FILE__) . '/lib/setup.php'); // Do not edit
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
