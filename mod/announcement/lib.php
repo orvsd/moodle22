5c80532286a4490d386972a3738fe9f740fd0bc4
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-02-20 15:39:26 -0800

    Revert "Updating announcement"
    This reverts commit b097e1deab694b6e0c5dc76a674bdb1e8fee2de1.

--- mod/announcement/lib.php
+++ mod/announcement/lib.php
@@ -65,14 +65,12 @@ function announcement_supports($feature) {
  * @param mod_announcement_mod_form $mform
  * @return int The id of the newly inserted announcement record
  */
-function announcement_add_instance($announcement) {
+function announcement_add_instance(stdClass $announcement, mod_announcement_mod_form $mform = null) {
     global $DB;
 
-    $announcement->name = get_announcement_name($announcement);
-    $announcement->timemodified = time();
+    $announcement->timecreated = time();
 
-
- 
+    # You may have to add extra stuff in here #
 
     return $DB->insert_record('announcement', $announcement);
 }
@@ -88,10 +86,9 @@ function announcement_add_instance($announcement) {
  * @param mod_announcement_mod_form $mform
  * @return boolean Success/Fail
  */
-function announcement_update_instance($announcement) {
+function announcement_update_instance(stdClass $announcement, mod_announcement_mod_form $mform = null) {
     global $DB;
 
-	$announcement->name = get_announcement_name($announcement);
     $announcement->timemodified = time();
     $announcement->id = $announcement->instance;
 
@@ -119,106 +116,278 @@ function announcement_delete_instance($id) {
 
     # Delete any dependent records here #
 
-    $result = true;
+    $DB->delete_records('announcement', array('id' => $announcement->id));
 
-    if (! $DB->delete_records("label", array("id"=>$label->id))) {
-        $result = false;
-    }
-
-    return $result;
+    return true;
 }
 
-
-
 /**
- * Given a course_module object, this function returns any
- * "extra" information that may be needed when printing
- * this activity in a course listing.
- * See get_array_of_activities() in course/lib.php
+ * Returns a small object with summary information about what a
+ * user has done with a given particular instance of this module
+ * Used for user activity reports.
+ * $return->time = the time they did it
+ * $return->info = a short text description
  *
- * @global object
- * @param object $coursemodule
- * @return object|null
+ * @return stdClass|null
  */
-function announcement_get_coursemodule_info($coursemodule) {
-    global $DB;
+function announcement_user_outline($course, $user, $mod, $announcement) {
 
-    if ($announcement = $DB->get_record('announcement', array('id'=>$coursemodule->instance), 'id, name, intro, introformat')) {
-        if (empty($announcement->name)) {
-            // announcement name missing, fix it
-            $announcement->name = "announcement{$announcement->id}";
-            $DB->set_field('announcement', 'name', $announcement->name, array('id'=>$announcement->id));
-        }
-        $info = new stdClass();
-        // no filtering hre because this info is cached and filtered later
-        $info->extra = format_module_intro('announcement', $announcement, $coursemodule->id, false);
-        $info->name  = $announcement->name;
-        return $info;
-    } else {
-        return null;
-    }
+    $return = new stdClass();
+    $return->time = 0;
+    $return->info = '';
+    return $return;
 }
 
 /**
- * @return array
- */
-function announcement_get_view_actions() {
-    return array();
-}
-
-/**
- * @return array
- */
-function announcement_get_post_actions() {
-    return array();
-}
-
-/**
- * This function is used by the reset_course_userdata function in moodlelib.
+ * Prints a detailed representation of what a user has done with
+ * a given particular instance of this module, for user activity reports.
  *
- * @param object $data the data submitted from the reset course.
- * @return array status array
+ * @param stdClass $course the current course record
+ * @param stdClass $user the record of the user we are generating report for
+ * @param cm_info $mod course module info
+ * @param stdClass $announcement the module instance record
+ * @return void, is supposed to echp directly
  */
-function announcement_reset_userdata($data) {
-    return array();
+function announcement_user_complete($course, $user, $mod, $announcement) {
 }
 
 /**
- * Returns all other caps used in module
+ * Given a course and a time, this module should find recent activity
+ * that has occurred in announcement activities and print it out.
+ * Return true if there was output, or false is there was none.
  *
+ * @return boolean
+ */
+function announcement_print_recent_activity($course, $viewfullnames, $timestart) {
+    return false;  //  True if anything was printed, otherwise false
+}
+
+/**
+ * Prepares the recent activity data
+ *
+ * This callback function is supposed to populate the passed array with
+ * custom activity records. These records are then rendered into HTML via
+ * {@link announcement_print_recent_mod_activity()}.
+ *
+ * @param array $activities sequentially indexed array of objects with the 'cmid' property
+ * @param int $index the index in the $activities to use for the next record
+ * @param int $timestart append activity since this time
+ * @param int $courseid the id of the course we produce the report for
+ * @param int $cmid course module id
+ * @param int $userid check for a particular user's activity only, defaults to 0 (all users)
+ * @param int $groupid check for a particular group's activity only, defaults to 0 (all groups)
+ * @return void adds items into $activities and increases $index
+ */
+function announcement_get_recent_mod_activity(&$activities, &$index, $timestart, $courseid, $cmid, $userid=0, $groupid=0) {
+}
+
+/**
+ * Prints single activity item prepared by {@see announcement_get_recent_mod_activity()}
+
+ * @return void
+ */
+function announcement_print_recent_mod_activity($activity, $courseid, $detail, $modnames, $viewfullnames) {
+}
+
+/**
+ * Function to be run periodically according to the moodle cron
+ * This function searches for things that need to be done, such
+ * as sending out mail, toggling flags etc ...
+ *
+ * @return boolean
+ * @todo Finish documenting this function
+ **/
+function announcement_cron () {
+    return true;
+}
+
+/**
+ * Returns all other caps used in the module
+ *
+ * @example return array('moodle/site:accessallgroups');
  * @return array
  */
 function announcement_get_extra_capabilities() {
-    return array('moodle/site:accessallgroups');
+    return array();
 }
 
-/**
- * @uses FEATURE_IDNUMBER
- * @uses FEATURE_GROUPS
- * @uses FEATURE_GROUPINGS
- * @uses FEATURE_GROUPMEMBERSONLY
- * @uses FEATURE_MOD_INTRO
- * @uses FEATURE_COMPLETION_TRACKS_VIEWS
- * @uses FEATURE_GRADE_HAS_GRADE
- * @uses FEATURE_GRADE_OUTCOMES
- * @param string $feature FEATURE_xx constant for requested feature
- * @return bool|null True if module supports feature, false if not, null if doesn't know
- */
-function announcement_supports($feature) {
-    switch($feature) {
-        case FEATURE_IDNUMBER:                return false;
-        case FEATURE_GROUPS:                  return false;
-        case FEATURE_GROUPINGS:               return false;
-        case FEATURE_GROUPMEMBERSONLY:        return true;
-        case FEATURE_MOD_INTRO:               return true;
-        case FEATURE_COMPLETION_TRACKS_VIEWS: return false;
-        case FEATURE_GRADE_HAS_GRADE:         return false;
-        case FEATURE_GRADE_OUTCOMES:          return false;
-        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
-        case FEATURE_BACKUP_MOODLE2:          return true;
-        case FEATURE_NO_VIEW_LINK:            return true;
+////////////////////////////////////////////////////////////////////////////////
+// Gradebook API                                                              //
+////////////////////////////////////////////////////////////////////////////////
 
-        default: return null;
+/**
+ * Is a given scale used by the instance of announcement?
+ *
+ * This function returns if a scale is being used by one announcement
+ * if it has support for grading and scales. Commented code should be
+ * modified if necessary. See forum, glossary or journal modules
+ * as reference.
+ *
+ * @param int $announcementid ID of an instance of this module
+ * @return bool true if the scale is used by the given announcement instance
+ */
+function announcement_scale_used($announcementid, $scaleid) {
+    global $DB;
+
+    /** @example */
+    if ($scaleid and $DB->record_exists('announcement', array('id' => $announcementid, 'grade' => -$scaleid))) {
+        return true;
+    } else {
+        return false;
     }
 }
 
+/**
+ * Checks if scale is being used by any instance of announcement.
+ *
+ * This is used to find out if scale used anywhere.
+ *
+ * @param $scaleid int
+ * @return boolean true if the scale is used by any announcement instance
+ */
+function announcement_scale_used_anywhere($scaleid) {
+    global $DB;
+
+    /** @example */
+    if ($scaleid and $DB->record_exists('announcement', array('grade' => -$scaleid))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Creates or updates grade item for the give announcement instance
+ *
+ * Needed by grade_update_mod_grades() in lib/gradelib.php
+ *
+ * @param stdClass $announcement instance object with extra cmidnumber and modname property
+ * @return void
+ */
+function announcement_grade_item_update(stdClass $announcement) {
+    global $CFG;
+    require_once($CFG->libdir.'/gradelib.php');
+
+    /** @example */
+    $item = array();
+    $item['itemname'] = clean_param($announcement->name, PARAM_NOTAGS);
+    $item['gradetype'] = GRADE_TYPE_VALUE;
+    $item['grademax']  = $announcement->grade;
+    $item['grademin']  = 0;
+
+    grade_update('mod/announcement', $announcement->course, 'mod', 'announcement', $announcement->id, 0, null, $item);
+}
+
+/**
+ * Update announcement grades in the gradebook
+ *
+ * Needed by grade_update_mod_grades() in lib/gradelib.php
+ *
+ * @param stdClass $announcement instance object with extra cmidnumber and modname property
+ * @param int $userid update grade of specific user only, 0 means all participants
+ * @return void
+ */
+function announcement_update_grades(stdClass $announcement, $userid = 0) {
+    global $CFG, $DB;
+    require_once($CFG->libdir.'/gradelib.php');
+
+    /** @example */
+    $grades = array(); // populate array of grade objects indexed by userid
+
+    grade_update('mod/announcement', $announcement->course, 'mod', 'announcement', $announcement->id, 0, $grades);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// File API                                                                   //
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Returns the lists of all browsable file areas within the given module context
+ *
+ * The file area 'intro' for the activity introduction field is added automatically
+ * by {@link file_browser::get_file_info_context_module()}
+ *
+ * @param stdClass $course
+ * @param stdClass $cm
+ * @param stdClass $context
+ * @return array of [(string)filearea] => (string)description
+ */
+function announcement_get_file_areas($course, $cm, $context) {
+    return array();
+}
+
+/**
+ * File browsing support for announcement file areas
+ *
+ * @package mod_announcement
+ * @category files
+ *
+ * @param file_browser $browser
+ * @param array $areas
+ * @param stdClass $course
+ * @param stdClass $cm
+ * @param stdClass $context
+ * @param string $filearea
+ * @param int $itemid
+ * @param string $filepath
+ * @param string $filename
+ * @return file_info instance or null if not found
+ */
+function announcement_get_file_info($browser, $areas, $course, $cm, $context, $filearea, $itemid, $filepath, $filename) {
+    return null;
+}
+
+/**
+ * Serves the files from the announcement file areas
+ *
+ * @package mod_announcement
+ * @category files
+ *
+ * @param stdClass $course the course object
+ * @param stdClass $cm the course module object
+ * @param stdClass $context the announcement's context
+ * @param string $filearea the name of the file area
+ * @param array $args extra arguments (itemid, path)
+ * @param bool $forcedownload whether or not force download
+ * @param array $options additional options affecting the file serving
+ */
+function announcement_pluginfile($course, $cm, $context, $filearea, array $args, $forcedownload, array $options=array()) {
+    global $DB, $CFG;
+
+    if ($context->contextlevel != CONTEXT_MODULE) {
+        send_file_not_found();
+    }
+
+    require_login($course, true, $cm);
+
+    send_file_not_found();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Navigation API                                                             //
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Extends the global navigation tree by adding announcement nodes if there is a relevant content
+ *
+ * This can be called by an AJAX request so do not rely on $PAGE as it might not be set up properly.
+ *
+ * @param navigation_node $navref An object representing the navigation tree node of the announcement module instance
+ * @param stdClass $course
+ * @param stdClass $module
+ * @param cm_info $cm
+ */
+function announcement_extend_navigation(navigation_node $navref, stdclass $course, stdclass $module, cm_info $cm) {
+}
+
+/**
+ * Extends the settings navigation with the announcement settings
+ *
+ * This function is called when the context for the page is a announcement module. This is not called by AJAX
+ * so it is safe to rely on the $PAGE.
+ *
+ * @param settings_navigation $settingsnav {@link settings_navigation}
+ * @param navigation_node $announcementnode {@link navigation_node}
+ */
+function announcement_extend_settings_navigation(settings_navigation $settingsnav, navigation_node $announcementnode=null) {
+}
