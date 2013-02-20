1b1297e56d6be0afea9795ae34c558a2ca602038
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-02-20 15:38:54 -0800

    Revert "More announcement updates"
    This reverts commit b22ca18587f7db3f831a6592ac978e07018219d3.

--- mod/announcement/view.php
+++ mod/announcement/view.php
@@ -29,39 +29,50 @@
 
 /// (Replace announcement with the name of your module and remove this line)
 
-require_once("../../config.php");
-
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
-$n  = optional_param('a', 0, PARAM_INT);  // announcement instance ID - it should be named as the first character of the module
+$n  = optional_param('n', 0, PARAM_INT);  // announcement instance ID - it should be named as the first character of the module
 
 if ($id) {
-    $PAGE->set_url('/mod/announcement/index.php', array('id'=>$id));
-    if (! $cm = get_coursemodule_from_id('announcement', $id)) {
-        print_error('invalidcoursemodule');
-    }
-
-    if (! $course = $DB->get_record("course", array("id"=>$cm->course))) {
-        print_error('coursemisconf');
-    }
-
-    if (! $announcement = $DB->get_record("announcement", array("id"=>$cm->instance))) {
-        print_error('invalidcoursemodule');
-    }
-
+    $cm         = get_coursemodule_from_id('announcement', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $announcement  = $DB->get_record('announcement', array('id' => $cm->instance), '*', MUST_EXIST);
+} elseif ($n) {
+    $announcement  = $DB->get_record('announcement', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $announcement->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('announcement', $announcement->id, $course->id, false, MUST_EXIST);
 } else {
-    $PAGE->set_url('/mod/announcement/index.php', array('l'=>$l));
-    if (! $announcement = $DB->get_record("announcement", array("id"=>$l))) {
-        print_error('invalidcoursemodule');
-    }
-    if (! $course = $DB->get_record("course", array("id"=>$announcement->course)) ){
-        print_error('coursemisconf');
-    }
-    if (! $cm = get_coursemodule_from_instance("announcement", $announcement->id, $course->id)) {
-        print_error('invalidcoursemodule');
-    }
+    error('You must specify a course_module ID or an instance ID');
 }
 
 require_login($course, true, $cm);
+$context = get_context_instance(CONTEXT_MODULE, $cm->id);
 
-redirect("$CFG->wwwroot/course/view.php?id=$course->id");
+add_to_log($course->id, 'announcement', 'view', "view.php?id={$cm->id}", $announcement->name, $cm->id);
+
+/// Print the page header
+
+$PAGE->set_url('/mod/announcement/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($announcement->name));
+$PAGE->set_heading(format_string($course->fullname));
+$PAGE->set_context($context);
+
+// other things you may want to set - remove if not needed
+//$PAGE->set_cacheable(false);
+//$PAGE->set_focuscontrol('some-html-id');
+//$PAGE->add_body_class('announcement-'.$somevar);
+
+// Output starts here
+echo $OUTPUT->header();
+
+if ($announcement->intro) { // Conditions to show the intro can change to look for own settings or whatever
+    echo $OUTPUT->box(format_module_intro('announcement', $announcement, $cm->id), 'generalbox mod_introbox', 'announcementintro');
+}
+
+// Replace the following lines with you own code
+echo $OUTPUT->heading('Yay! It works!');
+
+// Finish the page
+echo $OUTPUT->footer();
